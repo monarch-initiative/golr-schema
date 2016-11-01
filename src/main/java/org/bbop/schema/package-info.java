d75bdf3397b667fe57f7aa2e1698c3ec13c40ed5
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2016-11-01 15:30:39 -0700

    [#1] fix javadoc generation error

--- src/main/java/org/bbop/schema/package-info.java
+++ src/main/java/org/bbop/schema/package-info.java
@@ -3,9 +3,8 @@
  * 
  * <p>
  * This package supplies the tools necessary to parse, probe, and dump the YAML file configurations
- * for BBOP-JS constrained Solr indexes. Specifically, it can parse YAML files for use with the
- * {@link owltools.solrj.FlexSolrDocumentLoader} and take multiple YAML files and turn them into a
- * Solr schema that is compatible {@link owltools.yaml.golrconfig.SolrSchemaXMLWriter}.
+ * for BBOP-JS constrained Solr indexes. Specifically, it takes multiple YAML files and turn them
+ * into a Solr schema that is compatible {@link org.bbop.schema.SolrSchemaXMLWriter}.
  * </p>
  */
 package org.bbop.schema;
